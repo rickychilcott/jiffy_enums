e82928676410c0dd2d1e1eb87b78e36388da2a3a
James Joseph Houston Starr <jamesstarr@identified.com>
2012-09-11 12:22:52 -0700

    fixing generating coffee script

--- lib/jiffy_enums.rb
+++ lib/jiffy_enums.rb
@@ -1,14 +1,16 @@
 require "jiffy_enum"
 
 module JiffyEnums
-  include Enumerable
+  extend Enumerable
   # ordinals start from index 1 and NOT 0
   def define(key, value = nil, &overrides)
     @size = (@size || 0) + 1
     @hash ||= {}
+    @array ||= []
     the_enum = self.new(key, value, @size )
-    @hash[key] = the_enum
-    @hash[key.to_s] =the_enum
+    @hash[key]      = the_enum
+    @hash[key.to_s] = the_enum
+    @array << the_enum
     define_singleton_method(key) do
       the_enum
     end
@@ -21,13 +23,13 @@ module JiffyEnums
 
 
   def each
-    @hash.each do |key, value|
-      yield key, value
+    @array.each do |value|
+      yield value
     end
   end
 
   def all
-    @hash.values
+    @array.map{|x|x}
   end
 
   def for_value (value)
@@ -37,15 +39,13 @@ module JiffyEnums
   end
 
   def for_ordinal (ordinal)
-    all.select do |enum|
-      enum.ordinal == ordinal
-    end.first
+    @array[ordinal - 1]
   end
 
   def all_to_hash
     hash = {}
-    each do |key, value|
-      hash[key] = value.value
+    each do | value|
+      hash[value.key] = value.value
     end
     hash
   end
@@ -58,8 +58,8 @@ module JiffyEnums
     end
 
     final_coffee = final_coffee + "#{final_variable} = new JiffyEnums #{
-      @hash.map do |key, enum|
-        "#{key}:new JiffyEnum(\"#{enum.key}\", \"#{enum.value}\", #{enum.ordinal})"
+      @array.map do |enum|
+        "#{enum.key}:new JiffyEnum(\"#{enum.key}\", \"#{enum.value}\", #{enum.ordinal})"
       end.join(", ")
     }\n"
     final_coffee
